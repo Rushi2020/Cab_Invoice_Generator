826ea805812eba22d839a4b2a5ae0b456f49b0c3
Rushi <189489rishi@gmail.com>
2022-03-26 22:37:04 +0530

    calculate fare for a journey

---
+++ CabTesting/UnitTest1.cs
@@ -0,0 +1,36 @@
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cab_Invoice_Generator;
+using NUnit.Framework;
+//using System;
+
+
+namespace CabTesting
+{
+   
+    public class UnitTest1
+    {
+
+        public InvoiceGenerator invoiceGenerator;
+        [SetUp]
+        public void Setup()
+        {
+            invoiceGenerator = new InvoiceGenerator();
+        }
+
+        [Test]
+        public void givenDistanceAndTime_WhenToCalculateFare_ShouldReturnFare()
+        {
+            double fare = invoiceGenerator.GenerateFare(2, 5);
+            Assert.AreEqual(25, fare);
+        }
+
+        [Test]
+        public void givenDistanceAndTime_WhenToCalculateMinimumFare_ShouldReturnMinimumFare()
+        {
+            double fair = invoiceGenerator.GenerateFare(0, 0);
+            Assert.AreEqual(5, fair);
+        }
+
+
+    }
+}
