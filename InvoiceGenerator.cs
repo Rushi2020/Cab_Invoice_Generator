c8c632a3fd0cec79529a77513357c8f5d624703d
Rushi <189489rishi@gmail.com>
2022-03-26 22:33:00 +0530

    Add project files.

---
+++ InvoiceGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cab_Invoice_Generator
+{
+    public class InvoiceGenerator
+    {
+        const int COST_PER_KILOMETER = 10;
+        const int COST_PER_MINUTE = 1;
+        const int MINIMUM_FARE = 5;
+
+        
+
+        public double GenerateFare(double distance, int time)
+        {
+            var fair = (distance * COST_PER_KILOMETER) + (time * COST_PER_MINUTE);
+            if (fair > MINIMUM_FARE)
+            {
+                return fair;
+            }
+            return MINIMUM_FARE;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// cab invoicegenrator
+
+using System;
+
+namespace Cab_Invoice_Generator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the CabInvoiceGenerator problems");
+        }
+    }
+}
