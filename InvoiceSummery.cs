30707b74f384c75d1dc8d2aedb6a26ebdfa7b07d
Rushi <189489rishi@gmail.com>
2022-03-28 12:20:37 +0530

    adding avg fair and total fair

---
+++ InvoiceSummery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cab_Invoice_Generator
+{
+    public class InvoiceSummery
+    {
+        public int Total_NoOf_Rides { get; set; }
+        public double Avg_Fair { get; set; }
+        public double Total_Fair { get; set; }
+    }
+}
